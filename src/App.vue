e9d94282ba6b08381a1d9d613af08cf770a0bf1f
ucong2222 <dbrudrjf21@gmail.com>
2021-03-01 17:14:28 +0900

    vite 사용해서 새 프로젝트 생성 및 프로젝트에서 안쓰는 파일 제거

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit quos tempore quasi modi, aliquam, harum reprehenderit distinctio dicta expedita autem delectus laudantium cumque, placeat dignissimos ratione omnis tenetur? Deserunt, consequuntur.
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+})
+</script>
+
+<style>
+html, body {
+  margin:0;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
