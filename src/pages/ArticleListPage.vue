2b1f50444aca76eb4594c522d151c53a620400cf
ucong2222 <dbrudrjf21@gmail.com>
2021-03-02 12:38:58 +0900

    게시물 리스트 꾸미기

--- src/pages/ArticleListPage.vue
+++ src/pages/ArticleListPage.vue
@@ -1,7 +1,7 @@
 <template>
   <TitleBar>게시물 리스트</TitleBar>
   
-  <section class="section section-article-write-form  px-2">
+  <section class="section section-article-write-form px-2">
     <div class="container mx-auto">
       <form v-on:submit.prevent="checkAndWriteArticle">
         <FormRow title="제목">
@@ -19,10 +19,36 @@
     </div>
   </section>
 
-  <section class="section section-article-list">
+  <section class="section section-article-list px-2">
     <div class="container mx-auto">
-      <div v-bind:key="article.id" v-for="article in state.articles">
-        {{ article.id }} // {{ article.title }} // {{ article.body }}
+      <div class="mt-6" v-bind:key="article.id" v-for="article in state.articles">
+        <div class="px-10 py-6 bg-white rounded-lg shadow-md">
+          <div class="flex justify-between items-center">
+            <span class="font-light text-gray-600">
+              2021-02-24 10:20:30
+            </span>
+            <a href="#" class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500">
+              자유
+            </a>
+          </div>
+          <div class="mt-2">
+            <a href="#" class="text-2xl text-gray-700 font-bold hover:underline">
+              {{ article.title }}
+            </a>
+            <p class="mt-2 text-gray-600">
+              {{ article.body }}
+            </p>
+          </div>
+          <div class="flex justify-between items-center mt-4">
+            <a href="#" class="text-blue-500 hover:underline">자세히 보기</a>
+            <div>
+              <a href="#" class="flex items-center">
+                <img src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=731&amp;q=80" alt="avatar" class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block">
+                <h1 class="text-gray-700 font-bold hover:underline">홍길동</h1>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
